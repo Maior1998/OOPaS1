301469326d1d687ea38dfef23fd793bc3062f68e
Sergey Patoka <sergey.pat98@gmail.com>
2020-02-14 08:03:44 +0500

    Создание абстрактного класса. Продолжение войны с зависимостями.

---
+++ Strategy.Domain/Models/GameUnit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strategy.Domain.Models
+{
+    public abstract class GameUnit
+    {
+        /// <summary>
+        /// Координата X текущего юнита.
+        /// </summary>
+        public int X { get; set; }
+
+        /// <summary>
+        /// Координата Y текущего юнита.
+        /// </summary>
+        public int Y { get; set; }
+
+        /// <summary>
+        /// Игрок, управляющий юнитом.
+        /// </summary>
+        public Player Player { get; }
+    }
+}
